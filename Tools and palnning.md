58cf502c04764fef1a3d78ad6ff0d2eff1d04091
low-perry <dariohaxhiraj@hotmail.com>
2024-04-10 18:01:09 +0200

    Deleted uneccessary css files, orgonized the navbar and structure of the main page in App

--- Tools and palnning.md
+++ Tools and palnning.md
@@ -1,6 +1,14 @@
 # Frontend
+## Tools
 Using [vite](https://vitejs.dev/guide/) and react with typescript  for the frontend project
 `npm create vite@latest frontend -- --template react-ts`
+
+Using [React-Bootstrap](https://react-bootstrap.github.io/docs/getting-started/introduction)
+`npm install react-bootstrap bootstrap`
+
+
+
+## Development
 
 The pages , components and types go into the following directories:
 - [pages](frontend\src\pages)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,45 @@
-import './App.css'
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 import { initialProducts } from './data'
 
 function App() {
 
   return (
-    <>
-    <header>Typezone</header>
-    <main>
-      <ul>
-        {initialProducts.map((product) => (
-          <li key={product.slug}>
-            <img src={product.image} alt={product.name} className='product-img' />
-            <h2>{product.name}</h2>
-            <p>{product.price}</p>
+    <div className='d-flex flex-column vh-100'>
+      <header>
+        <Navbar bg='dark' variant='dark' expand='lg'>
+          <Container>
+            <Navbar.Brand>Typezone</Navbar.Brand>  
+          </Container>
+          <Nav>
+            <a href="/cart" className='nav-link'>
+              {/* <i className='fas fa-shopping-cart'></i> */}
+              Cart
+            </a>
+            <a href="/signin" className='nav-link'>
+              {/* <i className='fas fa-user'></i> */}
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
+      <main>
+        <Container className='mt-3'>
+          <Row>
+            {initialProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img src={product.image} alt={product.name} className='product-imgage' />
+                <h2>{product.name}</h2>
+                <p>{product.price}</p>
 
-          </li>
-        ))}
-      </ul>
-    </main>
-    <footer>All rights reserved</footer>
-    </>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </main>
+      <footer>
+        All rights reserved
+      </footer>
+    </div>
   )
 }
 
