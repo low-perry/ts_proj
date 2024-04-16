e27f5d177a164ebcce36e9ba512c57479527d428
low-perry <dariohaxhiraj@hotmail.com>
2024-04-16 16:17:38 +0200

    Implemented remove from cart

--- frontend/src/Store.tsx
+++ frontend/src/Store.tsx
@@ -32,7 +32,9 @@ const initialState: AppState ={
 
 }
 
-type Action = { type: 'SWITCH_MODE'} | {type: 'CART_ADD_ITEM'; payload: CartItem}
+type Action = { type: 'SWITCH_MODE'} 
+    | {type: 'CART_ADD_ITEM'; payload: CartItem}   
+    | { type: 'CART_REMOVE_ITEM'; payload: CartItem }
 
 function reducer(state: AppState, action: Action): AppState {
     switch(action.type) {
@@ -56,6 +58,13 @@ function reducer(state: AppState, action: Action): AppState {
             localStorage.setItem('cartItems', JSON.stringify(cartItems));
             return {...state, cart: {...state.cart, cartItems}};
         }
+        case 'CART_REMOVE_ITEM': {
+            const cartItems = state.cart.cartItems.filter(
+              (item: CartItem) => item._id !== action.payload._id
+            )
+            localStorage.setItem('cartItems', JSON.stringify(cartItems))
+            return { ...state, cart: { ...state.cart, cartItems } }
+          }
         
           
         default:
